cd0803d9337a6b365ef10527b213090437ebeb21
Danilo Moura <danilolmoura@gmail.com>
2020-06-12 19:36:30 -0300

    Add tests for Question.was_published_recently

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3y1#qrg^j5#6)ajni$e$0w7&ghi41bc)ogb_#65(e-620tv$!p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testserver']
 
 
 # Application definition
