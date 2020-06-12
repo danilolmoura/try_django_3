60de47afd28deb7814e485b1242b885b49b52c44
Danilo Moura <danilolmoura@gmail.com>
2020-06-12 01:42:57 -0300

    Creating polls and templates views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = 'polls/index.html'
+
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+
+	return render(request, template, context)
+
+def detail(request, question_id):
+	template = 'polls/detail.html'
+
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, template, {'question': question})
+
+def results(request, question_id):
+   response = "You're looking at the results of question %s."
+   return render(request, response)
+
+def vote(request, question_id):
+	response = "You're voting on question %s" % question_id
+	return render(request, response)
 
